f595993242d7a1e9d13185dc253175492c4536ea
RootPark <rmsdnjs518@gmail.com>
2023-07-15 18:03:46 +0900

    INIT) DB table init

---
+++ sql/create.sql
@@ -0,0 +1,19 @@
+DROP SCHEMA IF EXISTS metastage;
+CREATE SCHEMA IF NOT EXISTS metastage DEFAULT CHARACTER SET utf8;
+USE metastage;
+
+-- --------------------------------
+
+DROP TABLE IF EXISTS metastage.nftinfo;
+
+CREATE TABLE IF NOT EXISTS metastage.nftinfo (
+    tokenId INT,
+    ownerAddress VARCHAR(45),
+    ticketUri VARCHAR(200),
+    photoUri VARCHAR(200),
+    photoOpened TINYINT(1),
+    isRegistered TINYINT(1),
+    created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
+    PRIMARY KEY(tokenId)
+);
